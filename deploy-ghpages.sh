cd740b2a31e57d7649a54891fdcbccd629837e5f
ZhekaVasil <zheka_1408@mail.ru>
2018-04-30 01:34:27 +0300

    Changes

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo GHDeploy
+git add .
 git commit --allow-empty -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_PAGES_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
